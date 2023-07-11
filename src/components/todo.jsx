16ad1e4272c5316f32115d49ca9552043b696597
Alibaba2023 <126192888+Alibaba2023@users.noreply.github.com>
2023-07-11 12:36:08 +0430

    todo list added

---
+++ src/components/todo.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './style.css';
+
+const Todo = () => {
+  const toDoList = ['University', 'Microvese', 'English'];
+
+  return (
+    <div className='contianer'>
+      <h1>React Todo List</h1>
+      <p>The data of the list is going to save in local storage</p>
+      <input type="text" required placeholder='Enter the task title' />
+      <ul>
+        {toDoList.map((task) => {
+          return (
+            <li key={task} className='task-item'>
+              <samp>{task}</samp>
+              <button>Delete</button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Todo;
