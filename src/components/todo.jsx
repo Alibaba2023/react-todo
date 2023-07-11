ca9926c50eba9f95743413d37c7211327c8a9837
Alibaba2023 <126192888+Alibaba2023@users.noreply.github.com>
2023-07-11 13:29:09 +0430

    linter error fixed

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -2,21 +2,20 @@ import React from 'react';
 import './style.css';
 
 const toDoList = ['University', 'Microvese', 'English'];
+
 const Todo = () => {
   return (
-    <div className="contianer">
+    <div className="container">
       <h1>React Todo List</h1>
       <p>The data of the list is going to save in local storage</p>
       <input type="text" required placeholder="Enter the task title" />
       <ul>
-        {toDoList.map((task) => {
-          return (
-            <li key={task} className="task-item">
-              <samp>{task}</samp>
-              <button>Delete</button>
-            </li>
-          );
-        })}
+        {toDoList.map((task) => (
+          <li key={task} className="task-item">
+            <samp>{task}</samp>
+            <button type="button">Delete</button>
+          </li>
+        ))}
       </ul>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
@@ -7,6 +7,5 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
-
